44085ebf185f2fd62855b1f4815b583b06673fd3
AKHIL BALTHI <118684885+akhilrocz@users.noreply.github.com>
2025-01-07 18:10:11 +0530

    Time: 4 ms (86.21%) | Memory: 42.7 MB (8.27%) - LeetSync

--- 1524-string-matching-in-an-array/string-matching-in-an-array.java
+++ 1524-string-matching-in-an-array/string-matching-in-an-array.java
@@ -1,16 +1,30 @@
 import java.util.*;
 class Solution {
     public List<String> stringMatching(String[] words) {
-       Set<String> res = new HashSet<>();
-       Arrays.sort(words,Comparator.comparingInt(String::length));  //sort array by length
-    for(int i=0;i<words.length;i++){
-        for(int j=i+1;j<words.length;j++){
-            if(words[j].contains(words[i])){
-                res.add(words[i]);
-                break;
-            }
+        List<String> res = new ArrayList<>();
+ for(int i=0;i<words.length;i++)
+ {
+    for(int j=0;j<words.length;j++)
+    {
+        if(i!=j && words[j].contains(words[i])){
+             res.add(words[i]);
+             break;
         }
     }
-     return new ArrayList<>(res);
+
+ }
+ return res;
+
+    //    Set<String> res = new HashSet<>();
+    //    Arrays.sort(words,Comparator.comparingInt(String::length));  //sort array by length
+    // for(int i=0;i<words.length;i++){
+    //     for(int j=i+1;j<words.length;j++){
+    //         if(words[j].contains(words[i])){
+    //             res.add(words[i]);
+    //             break;
+    //         }
+    //     }
+    // }
+    //  return new ArrayList<>(res);
     }
 }
