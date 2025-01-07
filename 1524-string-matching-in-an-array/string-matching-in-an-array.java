1d7fe8ec9722506bc2f9f594bf9bae5657cae91c
AKHIL BALTHI <118684885+akhilrocz@users.noreply.github.com>
2025-01-07 18:19:32 +0530

    Time: 5 ms (41.61%) | Memory: 42.3 MB (37.89%) - LeetSync

--- 1524-string-matching-in-an-array/string-matching-in-an-array.java
+++ 1524-string-matching-in-an-array/string-matching-in-an-array.java
@@ -12,20 +12,7 @@ class Solution {
              break;
         }
     }
-
  }
  return res;
-
-    //    Set<String> res = new HashSet<>();
-    //    Arrays.sort(words,Comparator.comparingInt(String::length));  //sort array by length
-    // for(int i=0;i<words.length;i++){
-    //     for(int j=i+1;j<words.length;j++){
-    //         if(words[j].contains(words[i])){
-    //             res.add(words[i]);
-    //             break;
-    //         }
-    //     }
-    // }
-    //  return new ArrayList<>(res);
     }
 }
