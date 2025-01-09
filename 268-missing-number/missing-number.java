b8da51d58e7d553d62b10f1dfdc20edeb3e66835
AKHIL BALTHI <118684885+akhilrocz@users.noreply.github.com>
2025-01-09 22:46:18 +0530

    Time: 0 ms (100.00%) | Memory: 45.2 MB (64.00%) - LeetSync

---
+++ 268-missing-number/missing-number.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+       int n = nums.length;
+       int actualSum=0;
+       int exceptedSum=0;
+       for(int num:nums){
+        actualSum+=num;
+       }
+       exceptedSum=n*(n+1)/2;
+       int res = exceptedSum-actualSum;
+       return res;
+        }
+    }
+
+
+
+// class Solution {
+//   public int missingNumber(int[] nums) {
+//     int n = nums.length + 1; // Assuming sequence starts from 0 and goes up to n-1 (length of the array + 1 for missing number)
+//     // Check if the array is empty
+//     if (n == 1) return 0; // If empty, missing number is the first element (0)
+//     // Handle cases where all numbers might cancel out
+//     for (int i = 0; i < n; i++) {
+//       // Check if the current expected number (i) exists in the array
+//       if (Arrays.stream(nums).anyMatch(num -> num == i)) {
+//         continue; // If found, skip to next expected number
+//       } else {
+//         return i; // If not found, it's the missing number - return here
+//       }
+//     }
+//   }
+// }
