ed53e0c9ed3248240e9832647f309d4e4eb387d3
AKHIL BALTHI <118684885+akhilrocz@users.noreply.github.com>
2025-01-11 23:45:54 +0530

    Time: 4 ms (39.27%) | Memory: 51.2 MB (66.18%) - LeetSync

--- 2219-maximum-number-of-words-found-in-sentences/maximum-number-of-words-found-in-sentences.js
+++ 2219-maximum-number-of-words-found-in-sentences/maximum-number-of-words-found-in-sentences.js
@@ -5,10 +5,8 @@
 var mostWordsFound = function(sentences) {
     let maxWords=0;
     for(let i=0;i<sentences.length;i++){
-           sentences[i]=sentences[i].split(" ");
-           let eachSentenceWordsCount = sentences[i].length;
+           let eachSentenceWordsCount = sentences[i].split(" ").length;
            maxWords= Math.max(eachSentenceWordsCount,maxWords);
         }
-        return maxWords;
-        
+        return maxWords;    
 };
