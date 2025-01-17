c0001d63176c93fc11902379281fb391a3582866
AKHIL BALTHI <118684885+akhilrocz@users.noreply.github.com>
2025-01-17 21:47:16 +0530

    Time: 3 ms (97.90%) | Memory: 44.8 MB (59.33%) - LeetSync

---
+++ 2886-faulty-keyboard/faulty-keyboard.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String finalString(String s) {
+        StringBuilder res=new StringBuilder();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)!= 'i'){
+                res.append(s.charAt(i));
+            }else if(s.charAt(i)=='i'){
+                res =res.reverse();
+            }
+        }
+        return res.toString();
+    }
+}
