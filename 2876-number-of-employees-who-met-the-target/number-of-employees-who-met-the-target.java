69e9f6f063bae6c8cac301b2805b3f10aec43fcf
AKHIL BALTHI <118684885+akhilrocz@users.noreply.github.com>
2025-01-21 09:53:13 +0530

    Time: 3 ms (2.55%) | Memory: 43.2 MB (9.59%) - LeetSync

--- 2876-number-of-employees-who-met-the-target/number-of-employees-who-met-the-target.java
+++ 2876-number-of-employees-who-met-the-target/number-of-employees-who-met-the-target.java
@@ -1,12 +1,18 @@
 class Solution {
     public int numberOfEmployeesWhoMetTarget(int[] hours, int target) {
-        int count=0;
-        for(int i=0;i<hours.length;i++)
-        {
-            if(hours[i]>=target){
-                count++;
+        Map<Integer, Integer> hourCounts = new HashMap<>();
+
+        for (int hour : hours) {
+            hourCounts.put(hour, hourCounts.getOrDefault(hour, 0) + 1);
+        }
+
+        int count = 0;
+        for (int hour : hourCounts.keySet()) {
+            if (hour >= target) {
+                count += hourCounts.get(hour);
             }
         }
+
         return count;
     }
 }
