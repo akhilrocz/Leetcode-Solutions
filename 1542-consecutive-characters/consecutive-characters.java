b6e515b6cbbfde11896bccc360891fa7921feada
AKHIL BALTHI <118684885+akhilrocz@users.noreply.github.com>
2025-01-22 11:59:48 +0530

    Time: 1 ms (99.92%) | Memory: 42.1 MB (71.10%) - LeetSync

--- 1542-consecutive-characters/consecutive-characters.java
+++ 1542-consecutive-characters/consecutive-characters.java
@@ -3,14 +3,9 @@ class Solution {
         int maxLength = 1;
         int count = 1;
         for (int i = 0; i < s.length() - 1; i++) {
-            if (s.charAt(i) == s.charAt(i + 1)) {
-                count++;
-            } else {
-                maxLength = Math.max(count, maxLength);
-                count = 1;
-            }
+           count = s.charAt(i)==s.charAt(i+1)?count+1:1;
+           maxLength=Math.max(maxLength,count);
         }
-        maxLength = Math.max(maxLength, count);
         return maxLength;
     }
 }
