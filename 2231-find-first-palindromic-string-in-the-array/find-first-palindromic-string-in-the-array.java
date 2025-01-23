e7dacbedda81bdb00524648bd979250fc38a033d
AKHIL BALTHI <118684885+akhilrocz@users.noreply.github.com>
2025-01-23 23:59:31 +0530

    Time: 4 ms (28.59%) | Memory: 45.1 MB (60.56%) - LeetSync

--- 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.java
+++ 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.java
@@ -1,13 +1,11 @@
 class Solution {
-    public String firstPalindrome(String[] words) {    
-      for(String word:words){
-        StringBuilder s=new StringBuilder();
-        s.append(word);
-        s.reverse();
-        if(word.equals(s.toString())){
-            return word;
+    public String firstPalindrome(String[] words) {
+        for (String word : words) {
+            String reversed = new StringBuilder(word).reverse().toString();
+            if (word.equals(reversed)) {
+                return word;
+            }
         }
-      }
-      return "";
+        return "";
     }
 }
