444de0024422c43ab148ad9017746993313372cf
AKHIL BALTHI <118684885+akhilrocz@users.noreply.github.com>
2025-01-23 23:59:01 +0530

    Time: 5 ms (13.92%) | Memory: 45 MB (60.56%) - LeetSync

--- 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.java
+++ 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.java
@@ -1,12 +1,13 @@
 class Solution {
-    public String firstPalindrome(String[] words) {
-       for(String word:words){
-        String reversed = new StringBuilder(word).reverse().toString();
-        if(word.equals(reversed)){
+    public String firstPalindrome(String[] words) {    
+      for(String word:words){
+        StringBuilder s=new StringBuilder();
+        s.append(word);
+        s.reverse();
+        if(word.equals(s.toString())){
             return word;
         }
-       }
-       return "";
-
+      }
+      return "";
     }
 }
