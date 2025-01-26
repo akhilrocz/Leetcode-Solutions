5527c85d895f2e976e408e7d412676f1ff344bd9
AKHIL BALTHI <118684885+akhilrocz@users.noreply.github.com>
2025-01-26 20:07:50 +0530

    Time: 3 ms (32.99%) | Memory: 43.6 MB (10.08%) - LeetSync

--- 3428-find-the-xor-of-numbers-which-appear-twice/find-the-xor-of-numbers-which-appear-twice.java
+++ 3428-find-the-xor-of-numbers-which-appear-twice/find-the-xor-of-numbers-which-appear-twice.java
@@ -6,13 +6,12 @@ class Solution {
         }
 
        List<Integer> l =new ArrayList<>();
-
+       
         for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
               if (entry.getValue() == 2) {
                 l.add(entry.getKey());
             }
         }
-
         int xor = 0;
         for (int x : l) {
             xor ^= x;
