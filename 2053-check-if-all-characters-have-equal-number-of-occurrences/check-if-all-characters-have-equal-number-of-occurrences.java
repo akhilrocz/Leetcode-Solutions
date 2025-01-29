4a1871ea2e0197c68bacaa98f4e7830c92936fac
AKHIL BALTHI <118684885+akhilrocz@users.noreply.github.com>
2025-01-29 17:53:54 +0530

    Time: 8 ms (46.68%) | Memory: 42.3 MB (37.05%) - LeetSync

---
+++ 2053-check-if-all-characters-have-equal-number-of-occurrences/check-if-all-characters-have-equal-number-of-occurrences.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean areOccurrencesEqual(String s) {
+        Map<Character,Integer> map =new HashMap<>();
+        for(char c:s.toCharArray()){
+            map.put(c,map.getOrDefault(c,0)+1);
+        }
+        Set<Integer> set =new HashSet<>(map.values());
+       return set.size()<=1;
+    }
+}
