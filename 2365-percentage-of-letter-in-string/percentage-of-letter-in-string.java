55d1d8aaf501230e0fd4e9ca478d724e305354d2
AKHIL BALTHI <118684885+akhilrocz@users.noreply.github.com>
2025-02-15 23:51:57 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (16.93%) - LeetSync

--- 2365-percentage-of-letter-in-string/percentage-of-letter-in-string.java
+++ 2365-percentage-of-letter-in-string/percentage-of-letter-in-string.java
@@ -1,11 +1,12 @@
 class Solution {
     public int percentageLetter(String s, char letter) {
-        int count=0;
-        for(char c:s.toCharArray()){
-            if(c==letter) {
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == letter) {
                 count++;
             }
         }
-        return (int) Math.floor((double)count/s.length()*100);
+
+        return count * 100 / s.length();
     }
 }
