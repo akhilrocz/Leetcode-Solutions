e82c2804ff5ad1fce4e14aa5da7ced9780c15d5c
AKHIL BALTHI <118684885+akhilrocz@users.noreply.github.com>
2025-03-03 08:20:07 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (13.88%) - LeetSync

--- 3371-harshad-number/harshad-number.java
+++ 3371-harshad-number/harshad-number.java
@@ -1,13 +1,14 @@
 class Solution {
     public int sumOfTheDigitsOfHarshadNumber(int x) {
-        
+       int temp=x;
         int sum=0;
-        int temp=x;
-        while(temp>0){
-            int r =temp%10;
-            sum+=r;
-            temp/=10;
-        }
-        return x%sum==0?sum:-1;
+       while(temp>0){
+        int r = temp%10;
+        sum+=r;
+        temp/=10;
+       }
+
+       if(x%sum==0) return sum;
+       else return -1;
     }
 }
