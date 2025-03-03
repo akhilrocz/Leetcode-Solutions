526d1bf9ea9467cb720f7c7d43fe507e417dadff
AKHIL BALTHI <118684885+akhilrocz@users.noreply.github.com>
2025-03-03 19:20:15 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (98.41%) - LeetSync

--- 2427-first-letter-to-appear-twice/first-letter-to-appear-twice.java
+++ 2427-first-letter-to-appear-twice/first-letter-to-appear-twice.java
@@ -1,20 +1,15 @@
 class Solution {
     public char repeatedCharacter(String s) {
-     
-     Map<Character,Integer> map = new HashMap<>();
-     for(char c:s.toCharArray()){
+    Map<Character,Integer> map = new HashMap<>();
+    for(char c:s.toCharArray())
+    {
         if(map.containsKey(c)){
             return c;
         }
+
         map.put(c,1);
-     }
+    }
 
-     return '\0';
-
-
-
-
-
-        
+    return '\0';
     }
 }
