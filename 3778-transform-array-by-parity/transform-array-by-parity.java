6f07ea981a3871a508a8e95073b1d29f05a6a895
AKHIL BALTHI <118684885+akhilrocz@users.noreply.github.com>
2025-03-08 16:11:35 +0530

    Time: 2 ms (72.74%) | Memory: 44.8 MB (91.56%) - LeetSync

--- 3778-transform-array-by-parity/transform-array-by-parity.java
+++ 3778-transform-array-by-parity/transform-array-by-parity.java
@@ -1,5 +1,15 @@
 class Solution {
     public int[] transformArray(int[] nums) {
-       return Arrays.stream(nums).map(num->num%2==0?0:1).sorted().toArray();
+        int[] res= new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]%2==0){
+                res[i]=0;
+            }else{
+                res[i]=1;
+            }
+        }
+
+        Arrays.sort(res);
+        return res;
     }
 }
