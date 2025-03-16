82b88a34e29e1a2380bb5a63b9cf0499aac06db9
AKHIL BALTHI <118684885+akhilrocz@users.noreply.github.com>
2025-03-16 23:51:55 +0530

    Time: 3 ms (12.65%) | Memory: 40.8 MB (27.75%) - LeetSync

--- 2288-count-operations-to-obtain-zero/count-operations-to-obtain-zero.java
+++ 2288-count-operations-to-obtain-zero/count-operations-to-obtain-zero.java
@@ -4,13 +4,12 @@ class Solution {
         if(num1==0 || num2==0){
             return 0;
         }
-        while(num1>0){
+        while(num1>0 && num2>0){
             if(num1>=num2){
                 num1 = num1-num2; 
             }else{
                 num2 = num2-num1;
             }
-
             count++;
         }
 
