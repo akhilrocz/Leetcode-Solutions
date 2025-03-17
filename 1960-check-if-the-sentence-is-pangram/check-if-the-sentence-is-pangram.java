3f8752677a58c82aa36dc3409423734c65a09458
AKHIL BALTHI <118684885+akhilrocz@users.noreply.github.com>
2025-03-17 11:29:55 +0530

    Time: 3 ms (47.00%) | Memory: 42 MB (10.48%) - LeetSync

--- 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.java
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.java
@@ -2,14 +2,13 @@ class Solution {
     public boolean checkIfPangram(String sentence) {
         Set<Character> set = new HashSet<>();
 
-        for (int i = 0; i < sentence.length(); i++) {
-            char c = sentence.charAt(i);
+        for (char c : sentence.toCharArray()) {
             if (c >= 'a' && c <= 'z') {
                 set.add(c);
             }
         }
 
-        // to check every char appear atleast once
         return set.size() == 26;
+
     }
 }
