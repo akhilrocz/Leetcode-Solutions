e74da1c2acbff768a50655090ff4c283a4e2c893
AKHIL BALTHI <118684885+akhilrocz@users.noreply.github.com>
2025-03-17 11:26:21 +0530

    Time: 1 ms (85.03%) | Memory: 41 MB (76.65%) - LeetSync

--- 1490-generate-a-string-with-characters-that-have-odd-counts/generate-a-string-with-characters-that-have-odd-counts.java
+++ 1490-generate-a-string-with-characters-that-have-odd-counts/generate-a-string-with-characters-that-have-odd-counts.java
@@ -1,11 +1,11 @@
 class Solution {
     public String generateTheString(int n) {
         StringBuilder sb = new StringBuilder();
-        if (n % 2 == 1) {
-            sb.append("a".repeat(n));
-        } else {
+        if (n % 2 == 0) {
             sb.append("a".repeat(n - 1));
             sb.append("b");
+        } else {
+            sb.append("a".repeat(n));
         }
 
         return sb.toString();
