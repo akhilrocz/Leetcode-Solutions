d228bc5b9dacc4af17e4b1daad949393086136d1
AKHIL BALTHI <118684885+akhilrocz@users.noreply.github.com>
2025-03-25 17:29:27 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (72.98%) - LeetSync

--- 2243-check-if-all-as-appears-before-all-bs/check-if-all-as-appears-before-all-bs.java
+++ 2243-check-if-all-as-appears-before-all-bs/check-if-all-as-appears-before-all-bs.java
@@ -1,29 +1,14 @@
 class Solution {
     public boolean checkString(String s) {
-        List<Integer> aIndices = new ArrayList<>();
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == 'a') {
-                aIndices.add(i);
-            }
-        }
-
-        List<Integer> bIndices = new ArrayList<>();
+        boolean foundB = false;
         for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) == 'b') {
-                bIndices.add(i);
+                foundB = true;
+            } else if (s.charAt(i) == 'a' && foundB) {
+                return false;
             }
         }
-        boolean flag = true;
-        for (int i = 0; i < aIndices.size(); i++) {
-            for (int j = 0; j < bIndices.size(); j++) {
-                if (aIndices.get(i) > bIndices.get(j)) {
-                    flag = false;
-                    break;
-                }
-            }
-        }
-
-    return flag;
+        return true;
 
     }
 }
