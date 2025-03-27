0a1e170ba246f5f989117a7a31a24abe9d5d32ac
AKHIL BALTHI <118684885+akhilrocz@users.noreply.github.com>
2025-03-27 16:48:45 +0530

    Time: 1 ms (60.09%) | Memory: 44.2 MB (56.38%) - LeetSync

---
+++ 3246-check-if-bitwise-or-has-trailing-zeros/check-if-bitwise-or-has-trailing-zeros.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean hasTrailingZeros(int[] nums) {
+        int count=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]%2==0){
+                count++;
+            }
+        }
+
+        return count>=2;
+    }
+}
