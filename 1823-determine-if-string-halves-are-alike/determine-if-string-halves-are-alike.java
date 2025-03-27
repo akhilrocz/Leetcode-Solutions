aaddabd287be525dd7c2d2574b7d9f03face4110
AKHIL BALTHI <118684885+akhilrocz@users.noreply.github.com>
2025-03-27 14:02:04 +0530

    Time: 12 ms (5.18%) | Memory: 41.8 MB (76.36%) - LeetSync

--- 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.java
+++ 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.java
@@ -3,20 +3,20 @@ class Solution {
         int count1 = 0;
         int count2 = 0;
         for (int i = 0; i < s.length() / 2; i++) {
-            for (int j = s.length() / 2; j < s.length(); j++) {
-                if ((s.charAt(i) == 'a' || s.charAt(i) == 'e' || s.charAt(i) == 'i' || s.charAt(i) == 'o'
-                        || s.charAt(i) == 'u')
-                        || (s.charAt(i) == 'A' || s.charAt(i) == 'E' || s.charAt(i) == 'I' || s.charAt(i) == 'O'
-                                || s.charAt(i) == 'U')) {
-                    count1++;
-                }
+            if ((s.charAt(i) == 'a' || s.charAt(i) == 'e' || s.charAt(i) == 'i' || s.charAt(i) == 'o'
+                    || s.charAt(i) == 'u')
+                    || (s.charAt(i) == 'A' || s.charAt(i) == 'E' || s.charAt(i) == 'I' || s.charAt(i) == 'O'
+                            || s.charAt(i) == 'U')) {
+                count1++;
+            }
+        }
 
-                if ((s.charAt(j) == 'a' || s.charAt(j) == 'e' || s.charAt(j) == 'i' || s.charAt(j) == 'o'
-                        || s.charAt(j) == 'u')
-                        || (s.charAt(j) == 'A' || s.charAt(j) == 'E' || s.charAt(j) == 'I' || s.charAt(j) == 'O'
-                                || s.charAt(j) == 'U')) {
-                    count2++;
-                }
+        for (int j = s.length() / 2; j < s.length(); j++) {
+            if ((s.charAt(j) == 'a' || s.charAt(j) == 'e' || s.charAt(j) == 'i' || s.charAt(j) == 'o'
+                    || s.charAt(j) == 'u')
+                    || (s.charAt(j) == 'A' || s.charAt(j) == 'E' || s.charAt(j) == 'I' || s.charAt(j) == 'O'
+                            || s.charAt(j) == 'U')) {
+                count2++;
             }
         }
         return count1 == count2;
