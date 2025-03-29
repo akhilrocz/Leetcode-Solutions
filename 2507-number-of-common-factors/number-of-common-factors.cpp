f729881689daa1f7215b9366de23e5e20b7be9ed
AKHIL BALTHI <118684885+akhilrocz@users.noreply.github.com>
2025-03-29 16:54:46 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (82.04%) - LeetSync

---
+++ 2507-number-of-common-factors/number-of-common-factors.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int commonFactors(int a, int b) {
+        int count = 0;
+        int minimum = -1;
+        minimum = min(a, b);
+        for (int i = 1; i <= minimum; i++) {
+            if ((a % i == 0) && (b % i == 0)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+};
