bfac1ea056ccfd198cafeda2c3e9f124b264830c
AKHIL BALTHI <118684885+akhilrocz@users.noreply.github.com>
2025-04-02 22:03:27 +0530

    Time: 2 ms (64.84%) | Memory: 42 MB (99.91%) - LeetSync

--- 3428-find-the-xor-of-numbers-which-appear-twice/find-the-xor-of-numbers-which-appear-twice.java
+++ 3428-find-the-xor-of-numbers-which-appear-twice/find-the-xor-of-numbers-which-appear-twice.java
@@ -1,12 +1,13 @@
 class Solution {
     public int duplicateNumbersXOR(int[] nums) {
-        Arrays.sort(nums);
-       int xor=0;
-        for(int i=0;i<nums.length-1;i++){
-            if(nums[i]==nums[i+1]){
-                xor^=nums[i];
-            }
+     int xor=0;
+     Arrays.sort(nums);
+     for(int i=0;i<nums.length-1;i++){
+        if(nums[i]==nums[i+1]){
+            xor^=nums[i];
         }
-        return xor;  
+     }
+
+     return xor;
     }
 }
