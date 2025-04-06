fd16084842209342207cc6a851c9bed612fd634d
AKHIL BALTHI <118684885+akhilrocz@users.noreply.github.com>
2025-04-06 06:49:14 +0530

    Time: 2 ms (40.21%) | Memory: 43 MB (56.44%) - LeetSync

--- 3367-find-the-sum-of-encrypted-integers/find-the-sum-of-encrypted-integers.java
+++ 3367-find-the-sum-of-encrypted-integers/find-the-sum-of-encrypted-integers.java
@@ -1,23 +1,23 @@
 class Solution {
-    public int sumOfEncryptedInt(int[] nums) {   
-        int sum=0;
-        for(int i=0;i<nums.length;i++){
-            int maxDigit=0;
-            int temp =nums[i];
+    public int sumOfEncryptedInt(int[] nums) {
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int maxDigit = 0;
+            int temp = nums[i];
 
-            while(temp>0){
-                int digit=temp%10;  
-                maxDigit=Math.max(maxDigit,digit);
-                temp/=10;
+            while (temp > 0) {
+                int digit = temp % 10;  //get each digit
+                maxDigit = Math.max(maxDigit, digit);
+                temp /= 10;
             }
 
-            int encrypted=0;
+            int encrypted = 0;
             int len = String.valueOf(nums[i]).length();
-            for(int j=0;j<len;j++){
-                encrypted=encrypted*10+maxDigit;
+            for (int j = 0; j < len; j++) {
+                encrypted = encrypted * 10 + maxDigit;
             }
 
-            sum+=encrypted;
+            sum += encrypted;
         }
 
         return sum;
