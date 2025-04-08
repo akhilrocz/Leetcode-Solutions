bbef47c8cf6e014f42a20acd89d97382b6439f73
AKHIL BALTHI <118684885+akhilrocz@users.noreply.github.com>
2025-04-08 08:44:42 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (94.63%) - LeetSync

--- 1584-average-salary-excluding-the-minimum-and-maximum-salary/average-salary-excluding-the-minimum-and-maximum-salary.java
+++ 1584-average-salary-excluding-the-minimum-and-maximum-salary/average-salary-excluding-the-minimum-and-maximum-salary.java
@@ -2,6 +2,7 @@ class Solution {
     public double average(int[] salary) {
         int minimum = Integer.MAX_VALUE;
         int maximum = Integer.MIN_VALUE;
+        double sum = 0;
         for (int i = 0; i < salary.length; i++) {
             if (salary[i] > maximum) {
                 maximum = salary[i];
@@ -9,21 +10,10 @@ class Solution {
             if(salary[i]<minimum){
                 minimum = salary[i];
             }
+            sum+=salary[i];
         }
 
-        double sum = 0;
-        double noOfNonMaxandMinElements = 0;
-
-        for (int k = 0; k < salary.length; k++) {
-            if (salary[k] != maximum) {
-                if (salary[k] != minimum) {
-                    noOfNonMaxandMinElements++;
-                    sum += salary[k];
-                }
-            }
-        }
-
-        return sum / noOfNonMaxandMinElements;
-
+ 
+     return (sum-minimum-maximum)/(salary.length-2);
     }
 }
