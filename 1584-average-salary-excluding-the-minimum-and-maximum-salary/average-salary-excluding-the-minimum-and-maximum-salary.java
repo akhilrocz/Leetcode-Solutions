626204ae2f4c0e07bf15c6fc14e8d197ca67b03e
AKHIL BALTHI <118684885+akhilrocz@users.noreply.github.com>
2025-04-08 08:39:36 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (56.19%) - LeetSync

--- 1584-average-salary-excluding-the-minimum-and-maximum-salary/average-salary-excluding-the-minimum-and-maximum-salary.java
+++ 1584-average-salary-excluding-the-minimum-and-maximum-salary/average-salary-excluding-the-minimum-and-maximum-salary.java
@@ -2,32 +2,28 @@ class Solution {
     public double average(int[] salary) {
         int minimum = Integer.MAX_VALUE;
         int maximum = Integer.MIN_VALUE;
-        for(int i=0;i<salary.length;i++){
-            if(salary[i]>maximum){
+        for (int i = 0; i < salary.length; i++) {
+            if (salary[i] > maximum) {
                 maximum = salary[i];
             }
-        }
-
-        for(int j=0;j<salary.length;j++){
-            if(salary[j]!=maximum && salary[j]<minimum){
-                minimum = salary[j];
+            if(salary[i]<minimum){
+                minimum = salary[i];
             }
         }
 
+        double sum = 0;
+        double noOfNonMaxandMinElements = 0;
 
-        double sum=0;
-        double noOfNonMaxandMinElements =0;
-
-        for(int k=0;k<salary.length;k++){
-            if(salary[k]!=maximum){
-                if(salary[k]!=minimum){
+        for (int k = 0; k < salary.length; k++) {
+            if (salary[k] != maximum) {
+                if (salary[k] != minimum) {
                     noOfNonMaxandMinElements++;
-                    sum+=salary[k];
+                    sum += salary[k];
                 }
             }
         }
 
-        return sum/noOfNonMaxandMinElements;
+        return sum / noOfNonMaxandMinElements;
 
     }
 }
