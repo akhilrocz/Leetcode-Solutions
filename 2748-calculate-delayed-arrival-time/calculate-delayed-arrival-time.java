4314f993c41d8245b5932f250ea75e179f5a658a
AKHIL BALTHI <118684885+akhilrocz@users.noreply.github.com>
2025-04-27 07:39:56 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (28.08%) - LeetSync

--- 2748-calculate-delayed-arrival-time/calculate-delayed-arrival-time.java
+++ 2748-calculate-delayed-arrival-time/calculate-delayed-arrival-time.java
@@ -1,11 +1,10 @@
 class Solution {
     public int findDelayedArrivalTime(int arrivalTime, int delayedTime) {
-        if (arrivalTime + delayedTime == 24) {
+        if(arrivalTime+delayedTime<24){
+            return arrivalTime+delayedTime;
+        }else if(arrivalTime+delayedTime==24){
             return 0;
-        } else if (arrivalTime + delayedTime > 24) {
-            return arrivalTime + delayedTime - (24);
         }
-
-        return arrivalTime + delayedTime;
+        return arrivalTime+delayedTime-24;
     }
 }
