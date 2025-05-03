1aa3e8d0e6c2077696f93b339cd38ce5160876ad
AKHIL BALTHI <118684885+akhilrocz@users.noreply.github.com>
2025-05-03 07:10:15 +0530

    Time: 8 ms (8.52%) | Memory: 45 MB (6.56%) - LeetSync

--- 3353-existence-of-a-substring-in-a-string-and-its-reverse/existence-of-a-substring-in-a-string-and-its-reverse.java
+++ 3353-existence-of-a-substring-in-a-string-and-its-reverse/existence-of-a-substring-in-a-string-and-its-reverse.java
@@ -1,15 +1,18 @@
 class Solution {
     public boolean isSubstringPresent(String s) {
-        String rev="";
-        for(int i=s.length()-1;i>=0;i--){
-            rev+=s.charAt(i);
+        String rev = "";
+        for (int i = s.length() - 1; i >= 0; i--) {
+            rev += s.charAt(i);
         }
 
-        for(int j=0;j<s.length()-1;j++){
-            for(int k=0;k<rev.length()-1;k++){
-                 if(s.substring(j,j+2).equals(rev.substring(k,k+2))){
-                    return true;
-                 }
+        Set<String> substrings = new HashSet<>();
+        for (int j = 0; j < s.length() - 1; j++) {
+            substrings.add(s.substring(j, j + 2));
+        }
+
+        for (int k = 0; k < rev.length() - 1; k++) {
+            if (substrings.contains(rev.substring(k, k + 2))) {
+                return true;
             }
         }
 
