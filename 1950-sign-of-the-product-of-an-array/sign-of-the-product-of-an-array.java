cbd156c8199e67169e2bf94e1b192fdb6732d6cf
AKHIL BALTHI <118684885+akhilrocz@users.noreply.github.com>
2025-08-07 10:52:38 +0530

    Time: 1 ms (18.61%) | Memory: 44.5 MB (18.61%) - LeetSync

--- 1950-sign-of-the-product-of-an-array/sign-of-the-product-of-an-array.java
+++ 1950-sign-of-the-product-of-an-array/sign-of-the-product-of-an-array.java
@@ -1,14 +1,21 @@
 class Solution {
     public int arraySign(int[] nums) {
-int negativeCount=0;
-        for(int num:nums){
-            if(num<0){
+        int negativeCount = 0;
+        int zeroCount = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] < 0) {
                 negativeCount++;
-            }else if(num==0){
-                return 0;
+            } else if (nums[i] == 0) {
+                zeroCount++;
             }
         }
 
-      return negativeCount%2!=0?-1:1;
+        if (zeroCount == 0 && negativeCount % 2 != 0) {
+            return -1;
+        } else if (zeroCount == 0 && negativeCount % 2 == 0) {
+            return 1;
+        }
+        return 0;
+
     }
 }
