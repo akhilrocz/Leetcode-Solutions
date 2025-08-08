de2be3c62321fbcc41c210ed7af5062f1d6ab280
AKHIL BALTHI <118684885+akhilrocz@users.noreply.github.com>
2025-08-08 22:41:30 +0530

    Time: 1 ms (99.85%) | Memory: 46 MB (72.61%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -1,17 +1,27 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        Map<Integer,Integer> map = new HashMap<>();
-        for(int num:nums){
-            map.put(num,map.getOrDefault(num,0)+1);
-        }
+        // Map<Integer,Integer> map = new HashMap<>();
+        // for(int num:nums){
+        //     map.put(num,map.getOrDefault(num,0)+1);
+        // }
 
-        for(Map.Entry<Integer,Integer> entry:map.entrySet()){
-            if(entry.getValue()==1){
-                return entry.getKey();
-            }
-        }
+        // for(Map.Entry<Integer,Integer> entry:map.entrySet()){
+        //     if(entry.getValue()==1){
+        //         return entry.getKey();
+        //     }
+        // }
 
 
-        return -1;
+
+        //Linear Time complexity implementation
+
+        int xor=0;
+
+for(int num:nums){
+    xor^=num;
+}
+
+return xor;
+
     }
 }
