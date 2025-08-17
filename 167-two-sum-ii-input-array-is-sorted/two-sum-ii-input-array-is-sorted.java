df254f0f25fbd030c2ef0ae90ed13572e31b224e
AKHIL BALTHI <118684885+akhilrocz@users.noreply.github.com>
2025-08-17 17:18:45 +0530

    Time: 6 ms (16.20%) | Memory: 46.7 MB (94.49%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
@@ -1,12 +1,26 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
         //brute force
-        for(int i=0;i<numbers.length-1;i++){
-            for(int j=i+1;j<numbers.length;j++){
-                if(numbers[i]+numbers[j]==target){
-                    return new int[]{i+1,j+1};
-                }
+        // for(int i=0;i<numbers.length-1;i++){
+        //     for(int j=i+1;j<numbers.length;j++){
+        //         if(numbers[i]+numbers[j]==target){
+        //             return new int[]{i+1,j+1};
+        //         }
+        //     }
+        // }
+
+        // return null;
+
+        //optimized
+
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int i=0;i<numbers.length;i++){
+            int complement = target-numbers[i];
+            if(map.containsKey(complement)){
+                return new int[]{map.get(complement)+1,i+1};
             }
+
+            map.put(numbers[i],i);
         }
 
         return null;
