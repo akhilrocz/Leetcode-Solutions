b04a49ce45aa4053d9c29adf643197efc2d0722e
AKHIL BALTHI <118684885+akhilrocz@users.noreply.github.com>
2025-08-24 12:40:17 +0530

    Time: 0 ms (100.00%) | Memory: 46 MB (47.99%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,31 +1,31 @@
 class Solution {
-    public int[] searchRange(int[] nums, int target) {
-       int first = binarySearch(nums,target,true);
-       int last = binarySearch(nums,target,false);
 
-       return new int[]{first,last};
+    public int[] searchRange(int[] nums, int target) {
+        int first = binarySearch(nums, target, true);
+        int last = binarySearch(nums, target, false);
+        return new int[] { first, last };
     }
-       private int binarySearch(int[] nums, int target, boolean findFirst){
-        int left=0,right=nums.length-1;
-        int index=-1;
-        while(left<=right){
-            int mid = left+(right-left)/2;
-            if(nums[mid]==target){
-                index =mid;
-                if(findFirst){
-                    right =mid-1;
-                }else{
-                    left =mid+1;
+
+    private int binarySearch(int[] nums, int target, boolean findFirst) {
+        int left = 0;
+        int right = nums.length - 1;
+        int ans = -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                ans = mid;
+                if (findFirst) {
+                    right = mid - 1; //searching left for even there is another first element
+                } else {
+                    left = mid + 1;
                 }
-            }else if(nums[mid]<target)
-            {
-                left=mid+1;
-            }else{
-                right =mid-1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
             }
         }
-       
-        return index;
+        return ans;
+    }
 
-       }
 }
