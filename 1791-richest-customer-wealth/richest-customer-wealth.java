9dfd06cf3fef7f66b83b89b27658950b789c1e03
AKHIL BALTHI <118684885+akhilrocz@users.noreply.github.com>
2025-08-28 20:39:54 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (43.39%) - LeetSync

--- 1791-richest-customer-wealth/richest-customer-wealth.java
+++ 1791-richest-customer-wealth/richest-customer-wealth.java
@@ -1,23 +1,30 @@
 class Solution {
     public int maximumWealth(int[][] accounts) {
-        int maxWealth = Integer.MIN_VALUE;
-        List<Integer> l = new ArrayList<>();
-        for (int[] row : accounts) {
-            int sum = 0;
-            for (int val : row) {
-                sum += val;
-            }
+        List<Integer> res = new ArrayList<>();
+        
+       
+       for(int[] row:accounts){
+        int sum=0;
+        for(int element:row){
+            sum+=element;
+        }
+            res.add(sum);
+       }
 
-            l.add(sum);
+   
+
+
+        int max = Integer.MIN_VALUE;
+
+        for(int num:res)
+        {
+            if(num>max){
+                max=num;
+            }
         }
 
-        for (int k = 0; k < l.size(); k++) {
-            if (l.get(k) > maxWealth) {
-                maxWealth = l.get(k);
-            }
-        }
+        return max;
 
-        return maxWealth;
 
     }
 }
