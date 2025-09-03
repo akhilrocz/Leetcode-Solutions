1a1259528471194cd06a9baafab594a0cb7d2726
AKHIL BALTHI <118684885+akhilrocz@users.noreply.github.com>
2025-09-03 22:10:06 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (13.68%) - LeetSync

--- 1293-three-consecutive-odds/three-consecutive-odds.java
+++ 1293-three-consecutive-odds/three-consecutive-odds.java
@@ -1,17 +1,11 @@
 class Solution {
     public boolean threeConsecutiveOdds(int[] arr) {
-        boolean consecutiveOdds = false;
-        for (int i = 0; i < arr.length - 2; i++) {
+        for (int i = 0; i <= arr.length - 3; i++) {
             if (arr[i] % 2 != 0 && arr[i + 1] % 2 != 0 && arr[i + 2] % 2 != 0) {
-                consecutiveOdds = true;
-                break;
+                return true;
             }
         }
 
-        if (!consecutiveOdds) {
-            return false;
-        } else {
-            return true;
-        }
+        return false;
     }
 }
