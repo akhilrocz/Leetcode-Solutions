c79c50dee5150b74a32d33eadb6ea869268a19a8
AKHIL BALTHI <118684885+akhilrocz@users.noreply.github.com>
2025-09-14 12:36:33 +0530

    Time: 62 ms (22.50%) | Memory: 48.6 MB (15.79%) - LeetSync

--- 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.java
+++ 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.java
@@ -1,25 +1,27 @@
 class Solution {
     public String sortVowels(String s) {
         StringBuilder sb = new StringBuilder();
-        List<Integer> list = new ArrayList<>();
+        List<Character> list = new ArrayList<>();
         for (int i = 0; i < s.length(); i++) {
-            int ascii  = s.charAt(i);
-            if("aeiouAEIOU".indexOf(s.charAt(i))!=-1 ){
-                list.add(ascii);
+            char ch = s.charAt(i);
+            if ("aeiouAEIOU".indexOf(ch) != -1) {
+                list.add(ch);
             }
         }
 
-    Collections.sort(list);
+        Collections.sort(list);
 
-int index=0;
-for(int j = 0;j<s.length();j++){
-        if ("aeiouAEIOU".indexOf(s.charAt(j)) != -1){
-            sb.append((char) (int) list.get(index++));
-        }else{
-            sb.append(s.charAt(j));
+        int index = 0;
+        for (int j = 0; j < s.length(); j++) {
+            char ch = s.charAt(j);
+            if ("aeiouAEIOU".indexOf(ch) != -1) {
+                sb.append(list.get(index));
+                index++;
+            } else {
+                sb.append(ch);
+            }
         }
-    }
 
-    return sb.toString();
-}
+        return sb.toString();
+    }
 }
