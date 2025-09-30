5f96bda31d4b8ed42a34e35c3dd80e2bd62d2efb
AKHIL BALTHI <118684885+akhilrocz@users.noreply.github.com>
2025-09-30 21:56:57 +0530

    Time: 2 ms (97.35%) | Memory: 43.8 MB (77.90%) - LeetSync

--- 3471-minimum-average-of-smallest-and-largest-elements/minimum-average-of-smallest-and-largest-elements.java
+++ 3471-minimum-average-of-smallest-and-largest-elements/minimum-average-of-smallest-and-largest-elements.java
@@ -4,21 +4,13 @@ class Solution {
         Arrays.sort(nums);
         int left = 0;
         int right = n - 1;
-        double[] res = new double[n / 2];
-        int index = 0;
         double minAvg = Double.MAX_VALUE;
         while (left < right) {
             double average = (double) (nums[left] + nums[right]) / 2.0;
-            res[index++] = average;
+            minAvg = Math.min(average, minAvg);
             left++;
             right--;
             //goes up to half of arr
-        }
-
-        for (double num : res) {
-            if (num < minAvg) {
-                minAvg = num;
-            }
         }
 
         return minAvg;
