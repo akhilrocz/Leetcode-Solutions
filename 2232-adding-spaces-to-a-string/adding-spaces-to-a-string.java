82633793066426e1db830d546a40a941f6d5b425
AKHIL BALTHI <118684885+akhilrocz@users.noreply.github.com>
2025-10-12 22:27:13 +0530

    Time: 21 ms (89.69%) | Memory: 80.6 MB (67.59%) - LeetSync

--- 2232-adding-spaces-to-a-string/adding-spaces-to-a-string.java
+++ 2232-adding-spaces-to-a-string/adding-spaces-to-a-string.java
@@ -3,15 +3,18 @@ class Solution {
         StringBuilder sb = new StringBuilder();
         int n = s.length();
         int m = spaces.length;
+
         int index = 0;
         for (int i = 0; i < n; i++) {
-            if (index < m && spaces[index] == i) {
+            if (index<m && spaces[index] == i) {  // 0 != 8..... 8 == 8  
                 sb.append(' ');
                 index++;
             }
 
-            sb.append(s.charAt(i));
+                sb.append(s.charAt(i));
+            
         }
+
         return sb.toString();
     }
 }
