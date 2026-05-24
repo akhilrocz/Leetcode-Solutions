8ef77d7cf016a84cc76587733ea4199402079be7
AKHIL BALTHI <118684885+akhilrocz@users.noreply.github.com>
2026-05-24 09:59:55 +0530

    Time: 6 ms (1.07%) | Memory: 44.1 MB (11.34%) - LeetSync

--- 4305-check-adjacent-digit-differences/check-adjacent-digit-differences.java
+++ 4305-check-adjacent-digit-differences/check-adjacent-digit-differences.java
@@ -1,13 +1,17 @@
 class Solution {
     public boolean isAdjacentDiffAtMostTwo(String s) {
-      int n = s.length();
-      for(int i=0;i<n-1;i++){
-        int diff = Math.abs(s.charAt(i+1)-s.charAt(i));
-        if(diff>2){
-            return false;
-        }
-      }
+        // int n = s.length();
+        // for (int i = 0; i < n - 1; i++) {
+        //     int diff = Math.abs(s.charAt(i + 1) - s.charAt(i));
+        //     if (diff > 2) {
+        //         return false;
+        //     }
+        // }
+        // return true;
 
-      return true;
+        //2nd approach
+        int n =s.length();
+
+        return IntStream.range(0,n-1).allMatch(i->Math.abs(s.charAt(i+1)-s.charAt(i))<=2);
     }
 }
