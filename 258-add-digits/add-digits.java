c366f6f0fc2b11c0102429c9b6ce57e5df8375d8
AKHIL BALTHI <118684885+akhilrocz@users.noreply.github.com>
2026-07-04 07:34:19 +0530

    Time: 1 ms (97.88%) | Memory: 42.7 MB (44.38%) - LeetSync

--- 258-add-digits/add-digits.java
+++ 258-add-digits/add-digits.java
@@ -1,16 +1,28 @@
 class Solution {
     public int addDigits(int num) {
 
-        while(num>=10){  //11
-            int sum=0;
-            while(num>0){
-                int r=num%10;  //8  //3
-                sum+=r;  //8 //11  //2
-                num/=10;//3  //0
-            }
-            num=sum;
-        }
-        return num;
+        // Iterative solution
+        // while(num>=10){  //11
+        //     int sum=0;
+        //     while(num>0){
+        //         int r=num%10;  //8  //3
+        //         sum+=r;  //8 //11  //2
+        //         num/=10;//3  //0
+        //     }
+        //     num=sum;
+        // }
+        // return num;
+
+        //optimized solution  ==> Digital root concept
+
+        //any number can be represented by multiple of 9
+
+       // Eg: 9*0+1, 9*0+2.... 9*1+1 
+
+       //if number is zero return 0, else return num%9
+
+       return num==0?0:1+(num-1)%9;
+
 
     }
 }
