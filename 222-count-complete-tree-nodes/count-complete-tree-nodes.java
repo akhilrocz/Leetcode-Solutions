1664c3dbb019fb82d60e7a8a1fc65e9444f742fb
AKHIL BALTHI <118684885+akhilrocz@users.noreply.github.com>
2026-07-11 09:43:43 +0530

    Time: 7 ms (6.86%) | Memory: 49.6 MB (13.22%) - LeetSync

--- 222-count-complete-tree-nodes/count-complete-tree-nodes.java
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.java
@@ -15,8 +15,36 @@
  */
 class Solution {
     public int countNodes(TreeNode root) {
-        if(root==null) return 0;
+
+        //recursive solution
+        //  if(root==null) return 0;
         
-        return 1+countNodes(root.left)+countNodes(root.right);
+        // return 1+countNodes(root.left)+countNodes(root.right);
+
+        //queue based approach
+
+         if(root==null) return 0;
+
+        Queue<TreeNode> queue = new LinkedList<>();  //creating queue object
+
+        queue.offer(root);
+
+        int nodes=0;
+
+        while(!queue.isEmpty()){
+            TreeNode current =queue.poll();
+            nodes++;
+
+            if(current.left!=null){
+                queue.add(current.left);
+            }
+
+            if(current.right!=null){
+                queue.add(current.right);
+            }
+        }
+
+        return nodes;
+
     }
 }
