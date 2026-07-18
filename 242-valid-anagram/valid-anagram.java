736e1d192a30f58e8bae0cb5c2f0d10c69d9e3a6
AKHIL BALTHI <118684885+akhilrocz@users.noreply.github.com>
2026-07-19 05:26:47 +0530

    Time: 16 ms (20.77%) | Memory: 46.6 MB (25.36%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        Map<Character, Integer> map = new HashMap<>();
+        Map<Character, Integer> map1 = new HashMap<>();
+
+        s=s.trim().replaceAll("\u20AC","");
+
+        t=t.trim().replaceAll("\u20AC","");
+
+        if(s.length()!=t.length()) return false;
+
+        for (char c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        for (char ch : t.toCharArray()) {
+            map1.put(ch, map1.getOrDefault(ch, 0) + 1);
+        }
+
+        for (char key : map.keySet()) {
+            if (!map.get(key).equals(map1.get(key)) ) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
