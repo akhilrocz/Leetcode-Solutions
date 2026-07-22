c790da603029bca8b89d991e32999564314c3cad
AKHIL BALTHI <118684885+akhilrocz@users.noreply.github.com>
2026-07-22 22:33:00 +0530

    Time: 7 ms (57.23%) | Memory: 47.3 MB (16.88%) - LeetSync

--- 2524-largest-positive-integer-that-exists-with-its-negative/largest-positive-integer-that-exists-with-its-negative.java
+++ 2524-largest-positive-integer-that-exists-with-its-negative/largest-positive-integer-that-exists-with-its-negative.java
@@ -10,7 +10,7 @@ class Solution {
         }
 
         for(int key:map.keySet()){
-            if(key>0 && key>max && map.containsKey(-key)){
+            if(key>max && map.containsKey(-key)){
                 max=key;
             }
         }
