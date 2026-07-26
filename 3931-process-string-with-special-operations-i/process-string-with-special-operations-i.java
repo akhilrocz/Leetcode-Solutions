19f9981b8071d116cb3318cacf7bdbb778ca233d
AKHIL BALTHI <118684885+akhilrocz@users.noreply.github.com>
2026-07-26 21:10:03 +0530

    Time: 4 ms (71.39%) | Memory: 55.4 MB (43.11%) - LeetSync

--- 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.java
+++ 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.java
@@ -1,20 +1,23 @@
 class Solution {
     public String processStr(String s) {
-        StringBuilder result = new StringBuilder();
-        for(int i=0;i<s.length();i++){
-            if(Character.isLowerCase(s.charAt(i))){
-                result.append(s.charAt(i));
-            }else if(s.charAt(i)=='*' && result.length()>0){
-                result.deleteCharAt(result.length()-1);
-            }else if(s.charAt(i)=='#'){
-                result.append(result.toString());
-            }else if(s.charAt(i)=='b'){
-                result.append('b');
-            }else if(s.charAt(i)=='%'){
-                result.reverse();
-            }
+       StringBuilder sb = new StringBuilder();
+       int n =s.length();
+       for(int i=0;i<n;i++){
+        char c=s.charAt(i);
+        if(c!='*' && c!='#' && c!='%'){
+            sb.append(c);
+        }else if(c=='#'){
+            sb.append(sb.toString());
+        }else if(c=='%'){
+            sb.reverse();
         }
+       else if(sb.length()>0 && c=='*'){
+            sb.deleteCharAt(sb.length()-1);
+        }
+       }
 
-        return result.toString();
+       String result =sb.toString();
+
+       return result;
     }
 }
