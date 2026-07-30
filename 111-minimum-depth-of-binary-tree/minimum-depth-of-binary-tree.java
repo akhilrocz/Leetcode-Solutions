bf39e0baa9e670acedc85405cdec1045c6a08f04
AKHIL BALTHI <118684885+akhilrocz@users.noreply.github.com>
2026-07-30 22:46:58 +0530

    Time: 1 ms (99.91%) | Memory: 81.7 MB (94.91%) - LeetSync

--- 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.java
+++ 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.java
@@ -1,27 +1,56 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- * int val;
- * TreeNode left;
- * TreeNode right;
- * TreeNode() {}
- * TreeNode(int val) { this.val = val; }
- * TreeNode(int val, TreeNode left, TreeNode right) {
- * this.val = val;
- * this.left = left;
- * this.right = right;
- * }
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
     public int minDepth(TreeNode root) {
-        //DFS - Recursive approach
+        //bfs
+
         if(root==null) return 0;
-        if(root.left==null  && root.right==null) return 1;
-        if(root.left==null)
-           return minDepth(root.right)+1;
-           if(root.right==null)
-           return minDepth(root.left)+1;
-         return Math.min(minDepth(root.left),minDepth(root.right))+1;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+
+        queue.offer(root);
+
+        int depth=1;
+
+        while(!queue.isEmpty()){
+
+            int size =queue.size();  //1
+
+            for(int i=0;i<size;i++){
+
+            TreeNode node = queue.poll();  //3. //9 //20
+
+            if(node.left==null && node.right==null) return depth;
+
+            if(node.left!=null){
+
+                queue.offer(node.left); // [9]   //[null]
+            }
+
+            if(node.right!=null){
+                queue.offer(node.right); // [9,20].  [null]
+            }
+
+
+            }
+
+            depth++;
+
+        }
+
+        return depth;
     }
 }
