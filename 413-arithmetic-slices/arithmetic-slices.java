1fa89427a88dc44ddba383851e860c394f6bb2c5
AKHIL BALTHI <118684885+akhilrocz@users.noreply.github.com>
2026-08-15 20:11:30 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (10.76%) - LeetSync

---
+++ 413-arithmetic-slices/arithmetic-slices.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int numberOfArithmeticSlices(int[] nums) {
+        int n = nums.length;
+        if (n < 3)
+            return 0;
+        int totalCount = 0;
+        int currentCount = 0;
+        for (int i = 2; i < n; i++) {
+            if (nums[i] - nums[i - 1] == nums[i - 1] - nums[i - 2]) {
+                currentCount++;
+                totalCount += currentCount;
+            } else {
+                currentCount = 0;
+            }
+        }
+
+        return totalCount;
+
+    }
+}
